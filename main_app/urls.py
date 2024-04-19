ddea1afff6441e37944873f606050a26a9cc676f
jtnguyen45 <jtnguyen45@gmail.com>
2024-04-19 16:10:53 -0700

    Update styling and add finches index

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('about/', views.about),
+    path('finches/', views.finches_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -21,3 +21,11 @@ finches = [
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
