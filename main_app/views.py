9e8069f730b4d140e4a735c3c58bdfee7689278a
jtnguyen45 <jtnguyen45@gmail.com>
2024-04-22 14:21:30 -0700

    Add details page and routing

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Finch
 
 def home(request):
     return render(request, 'home.html')
@@ -7,6 +8,13 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
+    })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {
+        'finch' : finch
     })
