ea38accaebde44b7ee44bb08787745cc21a7d5c9
jtnguyen45 <jtnguyen45@gmail.com>
2024-04-22 14:11:09 -0700

    Add Finch model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Finch(models.Model):
+    SEX_OPTIONS = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+
+    name = models.CharField(max_length=100)
+    sex = models.CharField(choices=SEX_OPTIONS)
+    species = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    image = models.CharField(max_length=100)
--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,5 @@
 from django.shortcuts import render
 
-#Temporary Database - REMOVE AFTER ADDING CAT MODEL
-finches = [
-    {'name': 'Sweetie',
-     'sex': 'male',
-     'species': 'pine grosbeak',
-     'color': 'red',
-     'description' : 'Absurdly tame, allowing very close approach.'},
-    {'name': 'Honey',
-     'sex': 'female',
-     'species': 'American goldfinch',
-     'color': 'yellow',
-     'description': 'Flocks of goldfinches congregate in weedy fields, making musical and plaintive calls.'},
-    {'name': 'Kuku',
-     'sex': 'male',
-     'species': 'purple finch',
-     'color': 'brown',
-     'description': 'They feed up in trees and on the ground in open woods.'}
-]
-
 def home(request):
     return render(request, 'home.html')
 
